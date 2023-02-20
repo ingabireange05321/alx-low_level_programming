a95896c1162870dda220a1c83341d22077080f19
ingabireange05321 <ruzibizangel96@gmail.com>
2023-02-20 05:30:03 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main-entry point
+*return:always 1
+*/
+int main (void)
+{
+write(STDOUT,"and that piece of art is useful\"-Dora Korpar,2015-10-09\n,"59);
+return(1);
+}
