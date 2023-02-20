c4849e2ba8db264dd065b857ae1235310c0e4277
ingabireange05321 <ruzibizangel96@gmail.com>
2023-02-20 05:34:09 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 /**
-*main-entry point
-*return:always 1
-*/
-int main (void)
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
 {
-write(STDOUT,"and that piece of art is useful\"-Dora Korpar,2015-10-09\n,"59);
-return(1);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
